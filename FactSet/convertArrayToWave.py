f260f2cca182a0b92e8c491a3cc4f98978bb9dd7
deepamsshah <deepam25498@gmail.com>
2020-09-11 15:39:15 -0400

    added company-wise solutions

--- FactSet/convertArrayToWave.py
+++ FactSet/convertArrayToWave.py
@@ -1,28 +1,19 @@
-{
 #Initial Template for Python 3
 import math
 def main():
-    
     T=int(input())
-    
     while(T>0):
-        
-        
         N=int(input())
         
         A=[int(x) for x in input().split()]
         convertToWave(A,N)
         for i in A:
             print(i,end=" ")
-        
-        
         print()
-        
-       
         T-=1
 if __name__=="__main__":
     main()
-}
+
 ''' This is a function problem.You only need to complete the function given below '''
 #User function Template for python3
 #Complete this function
