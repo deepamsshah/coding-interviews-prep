5d788b680c6c317c370772fe2c15be7adf23e60e
deepamsshah <deepam25498@gmail.com>
2020-09-11 15:44:47 -0400

    added company-wise solutions

--- Samsung/merge2SortedLinkedLists.py
+++ Samsung/merge2SortedLinkedLists.py
@@ -1,4 +1,4 @@
-{
+
 #Initial Template for Python 3
 # Node Class
 class Node:
@@ -29,38 +29,7 @@ class LinkedList:
             print(curr_node.data,end=" ")
             curr_node=curr_node.next
         print(' ')
-if __name__ == '__main__':
-    t=int(input())
-    for cases in range(t):
-        n,m = map(int, input().strip().split())
-        a = LinkedList() # create a new linked list 'a'.
-        b = LinkedList() # create a new linked list 'b'.
-        nodes_a = list(map(int, input().strip().split()))
-        nodes_b = list(map(int, input().strip().split()))
-        for x in nodes_a:
-            a.append(x)
-        for x in nodes_b:
-            b.append(x)
-        a.head = merge(a.head,b.head)
-        a.printList()
-}
-''' This is a function problem.You only need to complete the function given below '''
-#User function Template for python3
-'''
-	Function to merge two sorted lists in one
-	using constant space.
-	
-	Function Arguments: head_a and head_b (head reference of both the sorted lists)
-	Return Type: head of the obtained list after merger.
-	{
-		# Node Class
-		class Node:
-		    def __init__(self, data):   # data -> value stored in node
-		        self.data = data
-		        self.next = None
-	}
-	Contributed By: Nagendra Jha
-'''
+
 def merge(head_a,head_b):
     #code here
     global a
@@ -78,3 +47,36 @@ def merge(head_a,head_b):
     for i in elements:
         a.append(i)
     return a.head
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n,m = map(int, input().strip().split())
+        a = LinkedList() # create a new linked list 'a'.
+        b = LinkedList() # create a new linked list 'b'.
+        nodes_a = list(map(int, input().strip().split()))
+        nodes_b = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)
+        for x in nodes_b:
+            b.append(x)
+        a.head = merge(a.head,b.head)
+        a.printList()
+
+''' This is a function problem.You only need to complete the function given below '''
+#User function Template for python3
+'''
+	Function to merge two sorted lists in one
+	using constant space.
+	
+	Function Arguments: head_a and head_b (head reference of both the sorted lists)
+	Return Type: head of the obtained list after merger.
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+	Contributed By: Nagendra Jha
+'''
